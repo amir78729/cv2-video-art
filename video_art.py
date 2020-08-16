41652319c15bbf4eff85c57d680652993318f462
amir78729 <amir78729@gmail.com>
2020-08-16 22:16:11 +0430

    graystile timeline

--- video_art.py
+++ video_art.py
@@ -26,7 +26,7 @@ def calculate_mean_pixel(img):
     return [r, g, b]
 
 cap = cv2.VideoCapture('only-man.mp4')
-# cap = cv2.VideoCapture('lick.mp4')
+cap = cv2.VideoCapture('giphy.mp4')
 # cap = cv2.VideoCapture('IMG_4998.MOV')
 # cap = cv2.VideoCapture('video_2020-08-16_00-37-23.mp4')
 # cap = cv2.VideoCapture('video_2020-08-16_00-42-02.mp4')
@@ -54,42 +54,83 @@ i = 0
 # print(calculate_mean_pixel(bw))
 
 # cv2.imshow('kjkj', calculate_mean_pixel(bw))
+total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+print(total_frames)
+
+width = total_frames
+height = round(width/4)
+max_width = 1000
+image_width = round(max_width/4)
+image_height = total_frames%max_width+max_width
+
+demo_array = np.zeros((height, width, 3), dtype=np.uint8)
+
+demo_array[:,:] = [0,0,0]
+demo_image = Image.fromarray(demo_array, 'RGB')
+demo_image_resized = demo_image.resize((image_height, image_width))
+# demo_image_resized.show()
 
 resolution = 200
 data = np.zeros((resolution, resolution, 3), dtype=np.uint8)
 mean_array = np.array([[0, 0, 0]])
+
 while True:
     i += 1
-    # print(i)
     ret, frame = cap.read()
     if ret == True:
-        # print(np.mean(frame))
         cv2.imshow('original video', frame)
         blurred = cv2.GaussianBlur(frame, (7,7), 0)
         # black_and_white = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+           
+
 
         array = np.array(blurred)
-        # mean = np.mean(array)
         mean_pixel = calculate_mean_pixel(array)
         for pixel in range(resolution):
-            # print(pixel)
             data[pixel] = mean_pixel
         result = Image.fromarray(data, 'RGB')
         result_name = 'resized_image.png'
         result.save(result_name)
         average_frame = cv2.imread(result_name)
-        
+
         # print color on frame
         font = cv2.FONT_HERSHEY_SIMPLEX 
-        org = (40, 100) 
-        fontScale = 0.5
+        org = (20, 100) 
+        fontScale = 0.4
         color = (255, 255, 255) 
         thickness = 1
-        average_frame = cv2.putText(average_frame, '('+str(int(mean_pixel[0]))+','+str(int(mean_pixel[1]))+','+str(int(mean_pixel[2]))+')', org, font, fontScale, color, thickness, cv2.LINE_AA) 
+        average_frame = cv2.putText(average_frame, 'frame #' + str(i) + ': (' + str(int(mean_pixel[0])) + ',' + str(int(mean_pixel[1])) + ',' + str(int(mean_pixel[2])) + ')', org, font, fontScale, color, thickness, cv2.LINE_AA) 
         #show frame
         cv2.imshow('modified video', average_frame)
 
         mean_array = np.append(mean_array, mean_pixel)
+
+        #timeline
+        # demo_image = Image.fromarray(demo_array, 'RGB')
+
+        # result.save('timeline.jpg')#
+        # timeline = cv2.imread('timeline.jpg')#
+
+        # demo_image_resized = demo_image.resize((image_height, image_width))#
+        # demo_array[:,i - 1] = mean_array[i]#
+        # open_cv_image = np.array(demo_image_resized) 
+
+
+        
+        demo_array[:,i - 1] = mean_array[i]
+        demo_image = Image.fromarray(demo_array, 'RGB')
+        demo_image_resized = demo_image.resize((image_height, image_width))
+        demo_image_resized.save('timeline.jpg')
+        timeline = cv2.imread('timeline.jpg')
+        # open_cv_image = np.array(demo_image_resized) 
+
+
+
+        
+        timeline = timeline[:, :, ::-1].copy() 
+        cv2.imshow('timeline', timeline)
+
         # Press Q on keyboard to  exit
         if cv2.waitKey(25) & 0xFF == ord('q'):
             break
@@ -108,17 +149,17 @@ mean_array = np.reshape(mean_array, (-1, 3))
 width = mean_array.shape[0]
 height = round(width/4)
 
-print(height, width)
+print(mean_array.shape[0])
+
+# print(height, width)
 data = np.zeros((height, width, 3), dtype=np.uint8)
 # data[0:256, 0:256] = [255, 0, 0] # red patch in upper left
 for pixel in range(0, width):
     data[:,pixel] = mean_array[pixel]
 
 img = Image.fromarray(data, 'RGB')
-max_width = 1000
-width = round(max_width/4)
-height = mean_array.shape[0]%max_width+max_width
-img = img.resize((height, width))
+
+img = img.resize((image_height, image_width))
 img.save('The Art From the Video.jpg')
 img.show()
 
